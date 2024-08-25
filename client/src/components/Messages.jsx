a4e46aa36a3580bf9da7e23c62a16ed5eab0586c
Andry Ariadi <andryariadi23@gmail.com>
2024-08-25 20:38:43 +0700

    update styling messages component

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -6,32 +6,52 @@ const Messages = () => {
   return (
     <>
       {open ? (
-        <div className="bg-tal-500 flex flex-col min-w-[30rem]">
+        <div className="bg-tal-500 flex flex-col gap-2 min-w-[30rem] h-full">
           {/* Top */}
-          <div className="bg-amber-500 flex items-center gap-2 px-8 py-3">
-            <span>To :</span>
-            <h1>Andry Ariadi</h1>
+          <div className="bg-secondary flex items-center gap-2 px-5 py-3">
+            <span className="text-neutral-400 text-sm">To:</span>
+            <h1 className="text-primary text-xl font-semibold">Andry Ariadi</h1>
           </div>
 
           {/* Center */}
-          <div className="bg-rose-500 flex flex-col flex-1">
-            <div>Andry</div>
-            <div>Andry</div>
+          <div className="bg-ros-500 flex flex-col flex-1 px-5 overflow-y-scroll scrollbar-hide">
+            {[...Array(10)].map((_, i) => (
+              <div className="chat chat-end" key={i}>
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                    <img alt="User" src="/noAvatar.png" />
+                  </div>
+                </div>
+                <div className="chat-bubble">I hate you!</div>
+                <div className="chat-footer opacity-50">Seen at 12:46</div>
+              </div>
+            ))}
+            <div className="chat chat-start">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img alt="User" src="/noAvatar.png" />
+                </div>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
           </div>
 
           {/* Bottom */}
-          <form className="bg-sky-500">
+          <form className="bg-sy-500 pb-2 px-5">
             <div className="bg-secondary p-3 rounded-lg flex items-center gap-2 border border-secondary hover:border-primary transition-all duration-300">
               <input type="text" placeholder="Type a message..." className="bg-transparent outline-none text-xs placeholder:text-xs flex-1" />
-              <IoIosSend size={20} className="text-gray-500" />
+              <IoIosSend size={20} className="text-gray-500 hover:text-primary transition-all duration-300 cursor-pointer" />
             </div>
           </form>
         </div>
       ) : (
         <div className="bg-tal-500 flex flex-col items-center justify-center gap-2 p-10 min-w-[30rem]">
-          <h1 className="text-3xl">Welcome Andry Ariadi</h1>
+          <h1 className="text-3xl">
+            Welcome <span className="text-primary font-bold">Andry Ariadi</span>
+          </h1>
           <h2 className="text-xl">Select a chat to start messaging</h2>
-          <IoLogoWechat size={70} className="text-gray-500" />
+          <IoLogoWechat size={70} className="text-gray-500 hover:text-primary transition-all duration-300" />
         </div>
       )}
     </>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 flex items-center text-neutral-200">
+    <div className="bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 flex items-center text-neutral-200 h-[85%] overflow-hidden">
       {/* Sidebar */}
       <Sidebar />
 
