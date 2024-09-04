0ce28cda5fd34640d3f1ac9bc2fdcd19dc289e8b
Andry Ariadi <andryariadi23@gmail.com>
2024-09-04 14:09:29 +0700

    update cors origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://talksy-andryariadi.vercel.app",
     credentials: true,
   })
 );
