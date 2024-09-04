3c9db4dbc29c28173c18258ace7c2a5eb19cade2
Andry Ariadi <andryariadi23@gmail.com>
2024-09-04 16:05:19 +0700

    update server lagi

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
@@ -14,6 +15,7 @@ const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+// Inisialisasi aplikasi Express
 app.use(express.json());
 app.use(
   cors({
@@ -22,18 +24,23 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(helmet()); // Menambahkan middleware helmet untuk keamanan
 
+// Rute API
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
 
+// Menyajikan file statis
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
-
+// Menjalankan server
 server.listen(port, () => {
-  connectToMongoDB();
-  console.log(`Server running on port ${port}`);
+  connectToMongoDB()
+    .then(() => {
+      console.log(`Server berjalan pada port ${port}`);
+    })
+    .catch((error) => {
+      console.error("Gagal terhubung ke MongoDB:", error);
+    });
 });
